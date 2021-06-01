9090cc34ea1bb89531a4da6e46468efe2fc4d0d5
Joanna <Joanna@USERs-MacBook.local>
2021-06-01 15:19:12 +0800

    Update Heroku port

--- app.js
+++ app.js
@@ -72,6 +72,6 @@ app.get("/about", function(req, res) {
 // -------------------------------
 // Listen
 // -------------------------------
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("The server is running on port 3000");
 });
